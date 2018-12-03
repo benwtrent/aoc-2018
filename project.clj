3c21ce7291dfa97523e21ad096d4bbe409f77b37
Benjamin Trent <4357155+benwtrent@users.noreply.github.com>
2018-12-03 13:19:53 -0600

    Day3

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject advent-of-code-2018 "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.9.0"]]
-  :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]]}})
+  :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]
+                                  [org.clojure/math.combinatorics "0.1.4"]]}})
